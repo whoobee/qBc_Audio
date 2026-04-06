fe6153627c0bbc30b97a64912228cec63c8c4c5d
whoobee <hubalau@gmail.com>
2026-04-06 22:20:51 +0200

    Remove temp files

--- install_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Install Python dependencies for qBc_Audio
-# openwakeword requires --no-deps because tflite-runtime has no wheel for Python 3.13 aarch64
-set -e
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-pip install -r "${SCRIPT_DIR}/requirements.txt"
-pip install openwakeword --no-deps
